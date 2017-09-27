22e9fa9ae97570fa5805b2b4bba5c43c8aa4a5e5
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2017-09-27 15:43:53 -0400

    Integrated ListNumber support into signed numeric arrays.

--- test/org/epics/pvdata/NumericArrayTest.java
+++ test/org/epics/pvdata/NumericArrayTest.java
@@ -8,18 +8,25 @@ package org.epics.pvdata;
 import junit.framework.TestCase;
 
 import org.epics.pvdata.factory.PVDataFactory;
+import org.epics.pvdata.pv.PVByteArray;
 import org.epics.pvdata.pv.PVDataCreate;
 import org.epics.pvdata.pv.PVDoubleArray;
+import org.epics.pvdata.pv.PVFloatArray;
 import org.epics.pvdata.pv.PVIntArray;
+import org.epics.pvdata.pv.PVLongArray;
 import org.epics.pvdata.pv.PVNumberArray;
+import org.epics.pvdata.pv.PVShortArray;
 import org.epics.pvdata.pv.ScalarType;
+import org.epics.util.array.ArrayByte;
 import org.epics.util.array.ArrayDouble;
+import org.epics.util.array.ArrayFloat;
 import org.epics.util.array.ArrayInt;
+import org.epics.util.array.ArrayLong;
+import org.epics.util.array.ArrayShort;
 import org.epics.util.array.CollectionNumbers;
 import org.epics.util.array.ListNumber;
 import static org.junit.Assert.*;
 import static org.hamcrest.Matchers.*;
-import org.junit.Test;
 
 public class NumericArrayTest extends TestCase {
 
@@ -29,8 +36,28 @@ public class NumericArrayTest extends TestCase {
         assertThat(pvArray.get(), instanceOf(ArrayDouble.class));
         pvArray.put(0, CollectionNumbers.toListDouble(0,1,2,3,4,5,6,7,8,9));
         assertThat(pvArray.get(), equalTo(CollectionNumbers.toListDouble(0,1,2,3,4,5,6,7,8,9)));
-        pvArray.put(2, CollectionNumbers.toListDouble(3,2));
+        pvArray.put(2, CollectionNumbers.toListFloat(3,2));
         assertThat(pvArray.get(), equalTo(CollectionNumbers.toListDouble(0,1,3,2,4,5,6,7,8,9)));
+    }
+
+    public void testPutFloatArray1() {
+        PVDataCreate factory = PVDataFactory.getPVDataCreate();
+        PVFloatArray pvArray = (PVFloatArray) factory.createPVScalarArray(ScalarType.pvFloat);
+        assertThat(pvArray.get(), instanceOf(ArrayFloat.class));
+        pvArray.put(0, CollectionNumbers.toListFloat(0,1,2,3,4,5,6,7,8,9));
+        assertThat(pvArray.get(), equalTo(CollectionNumbers.toListFloat(0,1,2,3,4,5,6,7,8,9)));
+        pvArray.put(2, CollectionNumbers.toListDouble(3,2));
+        assertThat(pvArray.get(), equalTo(CollectionNumbers.toListFloat(0,1,3,2,4,5,6,7,8,9)));
+    }
+
+    public void testPutLongArray1() {
+        PVDataCreate factory = PVDataFactory.getPVDataCreate();
+        PVLongArray pvArray = (PVLongArray) factory.createPVScalarArray(ScalarType.pvLong);
+        assertThat(pvArray.get(), instanceOf(ArrayLong.class));
+        pvArray.put(0, CollectionNumbers.toListLong(0,1,2,3,4,5,6,7,8,9));
+        assertThat(pvArray.get(), equalTo(CollectionNumbers.toListLong(0,1,2,3,4,5,6,7,8,9)));
+        pvArray.put(2, CollectionNumbers.toListInt(3,2));
+        assertThat(pvArray.get(), equalTo(CollectionNumbers.toListLong(0,1,3,2,4,5,6,7,8,9)));
     }
 
     public void testPutIntArray1() {
@@ -39,8 +66,28 @@ public class NumericArrayTest extends TestCase {
         assertThat(pvArray.get(), instanceOf(ArrayInt.class));
         pvArray.put(0, CollectionNumbers.toListInt(0,1,2,3,4,5,6,7,8,9));
         assertThat(pvArray.get(), equalTo(CollectionNumbers.toListInt(0,1,2,3,4,5,6,7,8,9)));
-        pvArray.put(2, CollectionNumbers.toListInt(3,2));
+        pvArray.put(2, CollectionNumbers.toListLong(3,2));
         assertThat(pvArray.get(), equalTo(CollectionNumbers.toListInt(0,1,3,2,4,5,6,7,8,9)));
+    }
+
+    public void testPutShortArray1() {
+        PVDataCreate factory = PVDataFactory.getPVDataCreate();
+        PVShortArray pvArray = (PVShortArray) factory.createPVScalarArray(ScalarType.pvShort);
+        assertThat(pvArray.get(), instanceOf(ArrayShort.class));
+        pvArray.put(0, CollectionNumbers.toListShort(new short[] {0,1,2,3,4,5,6,7,8,9}));
+        assertThat(pvArray.get(), equalTo(CollectionNumbers.toListShort(new short[] {0,1,2,3,4,5,6,7,8,9})));
+        pvArray.put(2, CollectionNumbers.toListInt(3,2));
+        assertThat(pvArray.get(), equalTo(CollectionNumbers.toListShort(new short[] {0,1,3,2,4,5,6,7,8,9})));
+    }
+
+    public void testPutByteArray1() {
+        PVDataCreate factory = PVDataFactory.getPVDataCreate();
+        PVByteArray pvArray = (PVByteArray) factory.createPVScalarArray(ScalarType.pvByte);
+        assertThat(pvArray.get(), instanceOf(ArrayByte.class));
+        pvArray.put(0, CollectionNumbers.toListByte(new byte[] {0,1,2,3,4,5,6,7,8,9}));
+        assertThat(pvArray.get(), equalTo(CollectionNumbers.toListByte(new byte[] {0,1,2,3,4,5,6,7,8,9})));
+        pvArray.put(2, CollectionNumbers.toListInt(3,2));
+        assertThat(pvArray.get(), equalTo(CollectionNumbers.toListByte(new byte[] {0,1,3,2,4,5,6,7,8,9})));
     }
 
     public void testPutNumericArray1() {
